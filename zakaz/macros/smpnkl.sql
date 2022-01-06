756ca8262ddf8b0d76f80aaf314a7ff7fe107df6
Evgeny Rudenko <rudenkoevgeny@gmail.com>
2022-01-06 05:46:55 +0300

    ABC analisys

---
+++ zakaz/macros/smpnkl.sql
@@ -0,0 +1,7 @@
+{% macro smpnkl() %}
+    (sum (sum_sal) over (partition by id_contractor, contractor_name order by sum_sal desc rows  unbounded preceding))/ sum (sum_sal) over (partition by id_contractor, contractor_name order by sum_sal desc rows between unbounded preceding and  unbounded following)*100
+{% endmacro %}
+
+{% macro fix_str(str) %}
+  "'" + str + "'"
+{% endmacro %}
