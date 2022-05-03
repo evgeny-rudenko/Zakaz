16a903ad5590f751cb010fb502da119eecd3f5df
Evgeny Rudenko <rudenkoevgeny@gmail.com>
2022-05-03 15:51:37 +0300

    запуск задачи dbt

--- zakaz/calculate.py
+++ zakaz/calculate.py
@@ -1,10 +1,12 @@
 # prefect flow
+import os
 from prefect import Flow
 from prefect.tasks.dbt import DbtShellTask
 from sqlalchemy import false, true
 
 with Flow(name="dbt_flow") as f:
-    task = DbtShellTask(overwrite_profiles=False, shell="bash", 
-    command="dbt run", return_all=true, log_stderr=true)
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    task = DbtShellTask(overwrite_profiles=False, shell="bash",
+    command="dbt run", return_all=true, log_stderr=true,profiles_dir="/home/evgeny/.dbt" )(command='dbt run')
 
-    out = f.run()
+out = f.run()
